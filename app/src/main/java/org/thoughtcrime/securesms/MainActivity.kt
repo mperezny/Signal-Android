d9526dc87a3018435d9aee3c2d5b2ed03cc419f7
mperezny <86840856+mperezny@users.noreply.github.com>
2026-05-12 17:27:19 -0400

    Update MainActivity.kt

--- app/src/main/java/org/thoughtcrime/securesms/MainActivity.kt
+++ app/src/main/java/org/thoughtcrime/securesms/MainActivity.kt
@@ -640,7 +640,7 @@ class MainActivity :
           },
           secondaryContent = {
             val listContainerColor = if (isSplitPane) {
-              Color.Black
+              SignalTheme.colors.colorSurface1
             } else {
               MaterialTheme.colorScheme.surface
             }
